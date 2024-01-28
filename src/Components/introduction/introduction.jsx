9785e9aa5f75cb163e0a0cb636a2e4d04a9b3060
Ali Kiadarbandsari <aliakd1122@gmail.com>
2024-01-28 20:37:24 +0330

    introduction

--- src/Components/introduction/introduction.jsx
+++ src/Components/introduction/introduction.jsx
@@ -1,9 +1,18 @@
 import React from "react"
 import "./introduction.css"
+import introduction from "../Assets/introduction-img.jpg"
 const Introduction = () => {
     return (
-        <div>
-
+        <div className="introduction">
+            <div className="introduction-items">
+                <h3>رویتک در اختیار شماست!</h3>
+                <p>خدمات طراحی سایت خودتون رو به “رویتک” بسپارید<br></br>
+                    اگر میخواید کسب و کارتون رو گسترده تر کنید!</p>
+                <button>نحوه سفارش</button>
+            </div>
+            <div className="introduction-img">
+                <img src={introduction} alt="" />
+            </div>
         </div>
 
     )
