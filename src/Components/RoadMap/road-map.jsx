bba60663bd310636490c6ce2087a80bb4b060632
Mohammad-PanahiNik <125298901+Mohammad-PanahiNik@users.noreply.github.com>
2024-02-19 21:54:34 +0330

    add text box for road map

--- src/Components/RoadMap/road-map.jsx
+++ src/Components/RoadMap/road-map.jsx
@@ -44,6 +44,30 @@ const RoadMap = () =>{
                 description={"قراردادن سایت روی سرور و تحویل به شما"}
                 />
             </div>
+            <div className="need-web container">
+                <h3 className="lh-lg">چرا کسب و کار شما به وبسایت نیاز دارد؟!</h3>
+                <p className="text-justify lh-lg mt-3">از جمله مزایای داشتن یک فروشگاه اینترنتی، میتوان به بازخورد گسترده و کمک به فروش 
+                بیشتر محصولات شما اشاره کرد. داشتن یک فروشگاه اینترنتی با طراحی و رابط کاربری خاص و ساده می تواند با فراهم کردن دسترسی ساده به محصولات شما به رونق بیشتر کسب کارتان کمک کند.
+                </p>
+            </div>
+            <div className="need-seen container">
+                <div className="row">
+                    <div className="col-md-6 col-xl-7 d-none d-md-block"></div> 
+                    <div className="text-seen col-12 col-md-6 col-xl-4">
+                        <h3 className="lh-lg">نیاز به دیده شدن دارید؟!</h3>
+                        <p className="text-justify lh-lg mt-3">داشتن یک وبسایت ساده و بهینه یکیاز بهترین راه های معرفی و بیان توانایی های شماست. کسانی که دوست دارند بیشتر درباره شما و یا محصول و خدماتی که ارایه میدهید بیشتر بدانند میتوانند از طریق آدرس وبسایت و حتی با اسکن یک QR کد از روی کارت ویزیت شما به تمام محتوایی که دوست دارید با آنها به اشتراک بگذارید دهید دسترسی داشته باشند.</p>
+                    </div>
+                </div>
+            </div>
+            <div className="need-seen container">
+                <div className="row">
+                    <div className="text-seen col-12 col-md-4 col-xl-3">
+                        <h3 className="lh-lg">نیاز به تبلیغات دارید؟!</h3>
+                        <p className="text-justify lh-lg mt-3">اگر نیاز به تبلیغ و معرفی خودتان دارید، یک وبسایت تبلیغاتی بهترین راه برای دیده شدن در سطح دنیای مجازی است.</p>
+                    </div>
+                    <div className="col-md-7 col-xl-8 d-none d-md-block"></div> 
+                </div>
+            </div>
         </div>
         </div>
     )
